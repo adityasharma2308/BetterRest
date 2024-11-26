bcbca299792c8b08db95bd9de2dd2af02ac3e9d4
Aditya Sharma <adityasharma220803@gmail.com>
2024-11-27 00:06:47 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aditya Sharma on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
